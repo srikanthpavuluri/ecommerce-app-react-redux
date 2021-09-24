d81de1620e362fe49ae43d460054e08b2418ff1d
Srikanth Pavuluri <srikanthpavuluri99@gmail.com>
2021-09-24 10:00:24 +0530

    Update productActions.js

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -3,13 +3,12 @@ import {
   PRODUCTS_FETCH_SUCCESS,
   PRODUCTS_FETCH_FAIL,
 } from "../actions/actionTypes";
-import { productURL } from "../urls";
 import axios from "axios";
 
 export const getProducts = (dispatch) => {
   dispatch({ type: PRODUCTS_FETCH_START });
   axios
-    .get(productURL)
+    .get('http://localhost:3000/products')
     .then((res) => dispatch({ type: PRODUCTS_FETCH_SUCCESS, payload: res.data }))
     .catch((err) => dispatch({ type: PRODUCTS_FETCH_FAIL, payload: err }));
 };
